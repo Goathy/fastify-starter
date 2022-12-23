2e44633297a9ee275a17a99231d68f1dad4cca10
Jakub Gałecki <goathy.dev@gmail.com>
2022-12-23 14:25:36 +0000

    feat(context): added request-context
    * Used `nanoid` library for generating request ids.

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import { env } from 'node:process'
 
 import type { TypeBoxTypeProvider } from '@fastify/type-provider-typebox'
 import { fastify as Fastify } from 'fastify'
+import { nanoid } from 'nanoid'
 
 import { createLogger } from './logger.js'
 
@@ -13,10 +14,12 @@ const fastify = Fastify({
       keywords: ['kind', 'modifier']
     }
   },
-  return503OnClosing: true
+  return503OnClosing: true,
+  genReqId: () => nanoid()
 }).withTypeProvider<TypeBoxTypeProvider>()
 
 await fastify.register(import('./core/config/config.js'))
+await fastify.register(import('./core/context/context.js'))
 
 fastify.get('/', async () => ({ status: 'ok' }))
 
