0151d1e468ec76c5bac6bcde65a56c96155beab5
Jakub Gałecki <goathy.dev@gmail.com>
2023-01-17 17:13:46 +0000

    feat(swagger): add basic swagger configuration

--- src/server.ts
+++ src/server.ts
@@ -20,6 +20,16 @@ const fastify = Fastify({
   genReqId: generateId()
 }).withTypeProvider<TypeBoxTypeProvider>()
 
+await fastify.register(import('@fastify/swagger'), {
+  mode: 'dynamic'
+})
+await fastify.register(import('@fastify/swagger-ui'), {
+  routePrefix: '/documentation',
+  uiConfig: {
+    docExpansion: 'full'
+  }
+})
+
 await fastify.register(import('./core/config/config.js'))
 
 fastify.get('/', async () => ({ status: 'ok' }))
